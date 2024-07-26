39b95422e6fc2d8657ff3e479a3421c996589461
Alyssa Willson <awillso2@nd.edu>
2024-07-26 16:07:58 -0400

    Small edits to figures:

--- 3_graphing.R
+++ 3_graphing.R
@@ -1,9 +1,6 @@
 # Author: N. Kroeze
 # Description: 
 
-library(tidyverse)
-library(here)
-
 (rm(list = ls()))
 
 source("src/append_functions.R")
@@ -20,8 +17,8 @@ traits$env_treatment <- paste0(ifelse(traits$salinity == 0, "low_sal", "high_sal
 traits <- traits[-which(traits$species == "empty"), ]  # Modify the original data frame
 
 # Filtered data (assuming missing values in tot_bgb are NA)
-traits_filtered <- traits %>% 
-  filter(!is.na(tot_bgb))
+traits_filtered <- traits |>
+  dplyr::filter(!is.na(tot_bgb))
 
 bgb_biomass$env_treatment <- NA  # Create a new column for env_treatment (filled with NA initially)
 for (i in 1:nrow(bgb_biomass)) {
@@ -32,16 +29,21 @@ for (i in 1:nrow(bgb_biomass)) {
     }
 }
 
-
-ggplot(bgb_biomass[bgb_biomass$segment_top==0,], aes(y = weight_g, x = factor(env_treatment), fill = factor(species))) +
-  geom_violin(position=position_dodge(0.90),scale = 'width',trim = T) + 
-  geom_boxplot(position=position_dodge(0.9),width = 0.25) +
-  theme_bw() +
-  ylim(0, 17.5) +
-  ggtitle("Belowground Biomass in TOP Sediment Layer by Environment") +
-  labs(x = "Environmental Conditions", y = "Belowground Biomass (g)", color = "Competition") + # Custom labels
-  scale_fill_manual(values = c("#EE964B","#19647E", "#ED254E"),labels = c("COMP", "SCAM","SPPA"))+ # Set custom colors
-  guides(fill = guide_legend(title = "Species"))
+bgb_biomass |>
+  dplyr::filter(segment_top == 0) |>
+  dplyr::mutate(env_treatment = dplyr::if_else(env_treatment == 'high_sal/high_elev', 'GCREW/\nlow inundation', env_treatment),
+                env_treatment = dplyr::if_else(env_treatment == 'high_sal/low_elev', 'GCREW/\nhigh inundation', env_treatment),
+                env_treatment = dplyr::if_else(env_treatment == 'low_sal/high_elev', 'Rhode River/\nlow inundation', env_treatment),
+                env_treatment = dplyr::if_else(env_treatment == 'low_sal/low_elev', 'Rhode River/\nhigh inundation', env_treatment)) |>
+  ggplot2::ggplot(ggplot2::aes(y = weight_g, x = factor(env_treatment), fill = factor(species))) +
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.90),scale = 'width',trim = T) + 
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.9),width = 0.25) +
+  ggplot2::theme_bw() +
+  ggplot2::ylim(0, 17.5) +
+  ggplot2::ggtitle("Belowground Biomass in TOP Sediment Layer by Environment") +
+  ggplot2::labs(x = "Environmental Conditions", y = "Belowground Biomass (g)", color = "Competition") + # Custom labels
+  ggplot2::scale_fill_manual(values = c("#EE964B","#19647E", "#ED254E"),labels = c("COMP", "SCAM","SPPA"))+ # Set custom colors
+  ggplot2::guides(fill = ggplot2::guide_legend(title = "Species"))
 
 
 unique_cohort <- sort(unique(mixture$TOP$cohort))
@@ -50,48 +52,51 @@ color_map <- setNames(colors, unique_cohort)
 
 
 for(i in names(bgb_biomass_layer)){
-  bgb_biomass_layer[[i]] <- bgb_biomass_layer[[i]] %>%
-    left_join(mixture[[i]], by = "pot_no")
+  bgb_biomass_layer[[i]] <- bgb_biomass_layer[[i]] |>
+    dplyr::left_join(mixture[[i]], by = "pot_no")
 }
 
 
 
-bgb_biomass_layer[["TOP"]] %>%
-  ggplot(aes(y=Depth.x, x=prop_scam, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
-  facet_wrap(~ env_treatment.x) +
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
+bgb_biomass_layer[["TOP"]] |>
+  dplyr::mutate(env_treatment.x = sub(pattern = '/', replacement = '/\n', x = env_treatment.x)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment.x, x=prop_scam, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
+  #ggplot2::facet_wrap(~ env_treatment.x) +
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
                                                   "corn\nancestral","corn\nmodern")) +
-  labs(y = "Depth", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
-  theme_bw()+
-  xlim(0,0.022) +
-  guides(fill = guide_legend(title ="Cohort",reverse=TRUE))
+  ggplot2::labs(y = "Environmental Treatment", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.022) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title ="Cohort",reverse=TRUE))
 
 
-bgb_biomass_layer[["MID"]] %>%
-  ggplot(aes(y=Depth.x, x=prop_scam, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  facet_wrap(~ env_treatment.x) +
-  scale_fill_manual(values = color_map,labels = c("Blackwater Ancestral","Blackwater Modern",
+bgb_biomass_layer[["MID"]] |>
+  dplyr::mutate(env_treatment.x = sub(pattern = '/', replacement = '/\n', x = env_treatment.x)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment.x, x=prop_scam, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  #ggplot2::facet_wrap(~ env_treatment.x) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("Blackwater Ancestral","Blackwater Modern",
                                                   "Corn Ancestral","Corn Modern")) +
-  labs(y = "Depth", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
-  theme_bw()+
-  xlim(0,0.01) +
-  guides(fill = guide_legend(title ="Cohort",reverse=TRUE))
+  ggplot2::labs(y = "Environmental Treatment", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.01) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title ="Cohort",reverse=TRUE))
 
-bgb_biomass_layer[["BTM"]] %>%
-  ggplot(aes(y=Depth.x, x=prop_scam, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  facet_wrap(~ env_treatment.x) +
-  scale_fill_manual(values = color_map,labels = c("Blackwater Ancestral","Blackwater Modern",
+bgb_biomass_layer[["BTM"]] |>
+  dplyr::mutate(env_treatment.x = sub(pattern = '/', replacement = '/\n', x = env_treatment.x)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment.x, x=prop_scam, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = T,drop = F) +  # Dodge violin plots slightly
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  #ggplot2::facet_wrap(~ env_treatment.x) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("Blackwater Ancestral","Blackwater Modern",
                                                   "Corn Ancestral","Corn Modern")) +  # Set color based on year
-  labs(y = "Depth", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
-  theme_bw()+
-  xlim(0,0.0019) +
-  guides(fill = guide_legend(title ="Cohort",reverse=TRUE))
+  ggplot2::labs(y = "Environmental Treatment", x = expression("Dry Weight Density SCAM under Competition (g/cm"^3*")")) + 
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.0019) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title ="Cohort",reverse=TRUE))
 
 # Comparison to SCAM Monocultures
 bgb <- bgb_biomass[bgb_biomass$species=="scam",]
@@ -114,38 +119,41 @@ bgb_TOP <- bgb[bgb$Depth == "TOP",]
 bgb_MID <- bgb[bgb$Depth == "MID",]
 bgb_BTM <- bgb[bgb$Depth == "BTM",]
 
-bgb_TOP %>%
-  ggplot(aes(y=Depth, x=weight_density, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  facet_wrap(~ env_treatment) +
-  scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
+bgb_TOP |>
+  dplyr::mutate(env_treatment = sub(pattern = '/', replacement = '/\n', x = env_treatment)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment, x=weight_density, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  #ggplot2::facet_wrap(~ env_treatment) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
                                                   "corn\nancestral","corn\nmodern")) +
-  labs(y = "Depth", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
-  theme_bw()+
-  xlim(0,0.022) +
-  guides(fill = guide_legend(title = "Cohort",reverse=TRUE)) 
+  ggplot2::labs(y = "Environmental Treatment", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.022) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title = "Cohort",reverse=TRUE)) 
 
-bgb_MID %>%
-  ggplot(aes(y=Depth, x=weight_density, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  facet_wrap(~ env_treatment) +
-  scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
+bgb_MID |>
+  dplyr::mutate(env_treatment = sub(pattern = '/', replacement = '/\n', x = env_treatment)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment, x=weight_density, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  #ggplot2::facet_wrap(~ env_treatment) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
                                                   "corn\nancestral","corn\nmodern")) +
-  labs(y = "Depth", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
-  theme_bw()+
-  xlim(0,0.01) +
-  guides(fill = guide_legend(title = "Cohort",reverse=TRUE)) 
+  ggplot2::labs(y = "Environmental Treatment", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.01) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title = "Cohort",reverse=TRUE)) 
 
-bgb_BTM %>%
-  ggplot(aes(y=Depth, x=weight_density, fill = cohort)) + 
-  geom_violin(position=position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
-  geom_boxplot(position=position_dodge(0.95),width = 0.25) +
-  facet_wrap(~ env_treatment) +
-  scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
+bgb_BTM |>
+  dplyr::mutate(env_treatment = sub(pattern = '/', replacement = '/\n', x = env_treatment)) |>
+  ggplot2::ggplot(ggplot2::aes(y=env_treatment, x=weight_density, fill = cohort)) + 
+  ggplot2::geom_violin(position=ggplot2::position_dodge(0.95),scale = 'width',trim = TRUE) +  # Dodge violin plots slightly
+  ggplot2::geom_boxplot(position=ggplot2::position_dodge(0.95),width = 0.25) +
+  #ggplot2::facet_wrap(~ env_treatment) +
+  ggplot2::scale_fill_manual(values = color_map,labels = c("blackwater\nancestral","blackwater\nmodern",
                                                   "corn\nancestral","corn\nmodern")) +
-  labs(y = "Depth", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
-  theme_bw()+
-  xlim(0,0.0019) +
-  guides(fill = guide_legend(title = "Cohort",reverse=TRUE)) 
+  ggplot2::labs(y = "Environmental Treatment", x = expression(Weight ~ Density~SCAM~Monoculture~(g/cm^{3}))) + # Custom labels
+  ggplot2::theme_bw()+
+  ggplot2::xlim(0,0.0019) +
+  ggplot2::guides(fill = ggplot2::guide_legend(title = "Cohort",reverse=TRUE)) 
